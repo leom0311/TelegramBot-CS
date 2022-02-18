96af42d966be310072e55139558ae753bda193d1
leom0311 <milosfilipovic0311@gmail.com>
2022-02-18 14:35:05 +0400

    rename class name

--- Bot/CryptoClass.cs
+++ Bot/CryptoClass.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 
 namespace Bot
 {
-        public class mp
+    public class PriceInfo
     {
-        public decimal price { get; set; }
+            public decimal price { get; set; }
     }
     public class USDs
-        {
-            public mp USD { get; set; } 
-        }
-        public class Crypto
-        {
+    {
+            public PriceInfo USD { get; set; } 
+    }
+    public class Crypto
+    {
             public int id { get; set; }
             public string name { get; set; }
             public DateTime last_updated { get; set; }
             public USDs quote { get; set; }
-        }
+     }
 
-        public class CryptoData
-        {
+     public class CryptoData
+     {
             public List<Crypto> Data { get; set; }
-        }
+     }
 }
 
