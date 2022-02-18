8e79b65db97c358347b13692d24a82c34a3c51a9
leom0311 <milosfilipovic0311@gmail.com>
2022-02-18 11:59:50 +0400

    Add project files.

---
+++ Bot/CryptoClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+        public class mp
+    {
+        public decimal price { get; set; }
+    }
+    public class USDs
+        {
+            public mp USD { get; set; } 
+        }
+        public class Crypto
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public DateTime last_updated { get; set; }
+            public USDs quote { get; set; }
+        }
+
+        public class CryptoData
+        {
+            public List<Crypto> Data { get; set; }
+        }
+}
+
---
+++ Bot/Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Bot
+{
+    class Program
+    {
+        static private string Token { get; } = "5240186629:AAFD3-VZxhpuFaeJt5kxURliyjuYe00J66w";
+        static private string API_KEY { get; } = "cb2ac294-d395-4788-8302-2286c00db261";
+        static readonly TelegramBotClient Bot = new TelegramBotClient(Token);
+
+        [Obsolete]
+        static void  Main(string[] args)
+        {
+            Bot.StartReceiving();
+            Bot.OnMessage += Bot_OnMessage;
+            Console.ReadLine();
+        }
+
+
+        [Obsolete]
+        private static  void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string text = e.Message.Text;
+            CryptoData crypto = GetExternalResponse();
+            if (text.StartsWith("/") && text.Split(new char[] {'/'})[1] == "crypto")
+            {
+
+                var rkm = new ReplyKeyboardMarkup();
+                var rows = new List<KeyboardButton[]>();
+                var cols = new List<KeyboardButton>();
+                foreach (Crypto p in crypto.Data)
+                {
+                    string name = p.name;
+                    cols.Add(new KeyboardButton("" + name));
+                    rows.Add(cols.ToArray());
+                    cols = new List<KeyboardButton>();
+                }
+                rkm.Keyboard = rows.ToArray();
+                Bot.SendTextMessageAsync(
+                   e.Message.Chat.Id,
+                   "Choose one of these cryptovalues",
+                   replyMarkup: rkm);
+            }
+            else
+            {
+                foreach (Crypto p in crypto.Data)
+                {
+                    if (e.Message.Text == p.name)
+                    {
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id,p.quote.USD.price.ToString()+" USD");
+                    }
+                }
+            }
+        }
+        private static CryptoData GetExternalResponse()
+        {
+            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("Accepts", "application/json");
+            var result =  client.DownloadString(URL.ToString());
+            return JsonConvert.DeserializeObject<CryptoData>(result);
+        }
+
+    }
+   
+}
+
